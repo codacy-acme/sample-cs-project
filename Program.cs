9c3566bc0ef45d19790760186ec564bf8e7c5dc8
Hélio Rocha <hjrocha@gmail.com>
2020-01-03 09:32:16 +0000

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
